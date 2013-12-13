e2c12798ccf1761d33d8122248c4bc43caec2b84
Russell <russ.may@gmail.com>
2013-12-13 09:21:21 -0700

    Working if else block in elisp

--- .emacs.d/el-rusticus.el
+++ .emacs.d/el-rusticus.el
@@ -6,7 +6,7 @@
 (ido-mode t)
 
 ;; Set the starting position and width and height of Emacs Window
-(setq initial-frame-alist '((top . 10) (left . 30) (width . 120) (height . 40) ))
+(setq initial-frame-alist '((top . 10) (left . 30) (width . 120) (height . 35) ))
 
 ;; Prefer utf-8 encoding
 (prefer-coding-system 'utf-8)
@@ -62,39 +62,99 @@
 ;; smooth scrolling
 (require 'smooth-scrolling)
 
+;; Using my initials to avoid possible namespace collisions
+(require 'linum)
+(add-hook 'rdm-code-modes-hook
+          (lambda () (linum-mode 1)))
+(add-hook 'ruby-mode-hook
+          (lambda () (run-hooks 'rdm-code-modes-hook)))
+(add-hook 'html-mode-hook
+          (lambda () (run-hooks 'rdm-code-modes-hook)))
+(add-hook 'text-mode-hook
+          (lambda () (run-hooks 'rdm-code-modes-hook)))
 
-;; Org-mode Settings Begin
-(add-to-list 'load-path "c:/org/org-8.0.7/lisp")
-(add-to-list 'load-path "c:/org/org-8.0.7/contrib/lisp")
+;; "y or n" instead of "yes or no"
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; Explicitly show the end of a buffer
+(set-default 'indicate-empty-lines t)
+
+;;; Highlight current line
+(global-hl-line-mode 1)
+;; Great for white background
+(set-face-background 'hl-line "lemon chiffon")
+
+;;; Auto revert mode
+(global-auto-revert-mode 1)
+
+;; If I'm on Windows
+(if (eq system-type 'windows-nt)
+  (progn
+    ;; Use aspell instead of ispell
+    (setq ispell-program-name "C:/Program Files (x86)/Aspell/bin/aspell.exe")
+    (setq ispell-really-aspell t)
+    (setq ispell-extra-args '("--sug-mode=fast"))
+
+    ;; Org-mode Settings Begin
+    (add-to-list 'load-path "c:/org/org-8.0.7/lisp")
+    (add-to-list 'load-path "c:/org/org-8.0.7/contrib/lisp")
+    (setq org-agenda-file "c:/Users/Russell/SkyDrive/Dropbox/org/tasks.org")
+    (setq org-agenda-archive "c:/Users/Russell/SkyDrive/Dropbox/org/archive_personal.org::")
+
+    ;; Capture
+
+    ;; Commented out to try deft
+    ;(global-set-key (kbd "<f9>") 'org-capture)
+    ;(setq org-capture-templates
+    ;'(("t" "Agenda Todo" entry
+    ;	(file+headline "c:/Users/Russell/Dropbox/org/personal.org" "Agenda")
+    ;	 "\n\n** TODO %?\n%T\n\n%i\n%a\n\n\n"
+    ;	 :empty-lines 1)
+
+    ;("j" "Journal" entry (file+datetree "c:/Users/Russell/Dropbox/org/journal.org")
+    ;             "* %?\nEntered on %U\n  %i\n  %a")
+
+    ;	("n" "Agenda Notes" entry
+    ;	 (file+headline "c:/Users/Russell/Dropbox/org/personal.org" "Agenda")
+    ;	 "\n\n** %?\n%T\n%i\n%a\n\n\n"
+    ;	 :empty-lines 1)))
+  
+    ;; Set diary location
+    (setq diary-file "C:/Users/Russell/SkyDrive/Dropbox/files/work/diary")
+    
+    (setq deft-dir "c:/Users/Russell/SkyDrive/Dropbox/PlainText/")
+  
+    (find-file "c:/Users/Russell/SkyDrive/Dropbox/org/tasks.org")
+    (find-file "c:/Users/Russell/SkyDrive/Dropbox/org/personal.org")
+  )
+  (progn ; else
+    (setq org-agenda-file "/home/russell/Dropbox/org/tasks.org")
+    (setq org-agenda-archive "/home/russell/Dropbox/org/archive_personal.org::")
+    (setq deft-dir "/home/russell/Dropbox/PlainText/")
+  )
+)
+
+;; Org mode
 (require 'org)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (add-to-list 'auto-mode-alist '("\\todo.txt\\'" . org-mode))
-(setq org-agenda-files (list "c:/Users/Russell/SkyDrive/Dropbox/org/tasks.org"))
-(setq org-archive-location "c:/Users/Russell/SkyDrive/Dropbox/org/archive_personal.org::")
-'(org-agenda-files (quote ("c:/Users/Russell/SkyDrive/Dropbox/org/tasks.org")))
+(setq org-agenda-files (list org-agenda-file))
+(setq org-archive-location org-agenda-archive )
 (add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 (setq org-log-done t)
 
-;; Capture
-
-;; Commented out to try deft
-;(global-set-key (kbd "<f9>") 'org-capture)
-;(setq org-capture-templates
-;'(("t" "Agenda Todo" entry
-;	(file+headline "c:/Users/Russell/Dropbox/org/personal.org" "Agenda")
-;	 "\n\n** TODO %?\n%T\n\n%i\n%a\n\n\n"
-;	 :empty-lines 1)
-
-;("j" "Journal" entry (file+datetree "c:/Users/Russell/Dropbox/org/journal.org")
-;             "* %?\nEntered on %U\n  %i\n  %a")
-
-;	("n" "Agenda Notes" entry
-;	 (file+headline "c:/Users/Russell/Dropbox/org/personal.org" "Agenda")
-;	 "\n\n** %?\n%T\n%i\n%a\n\n\n"
-;	 :empty-lines 1)))
+;; Deft
+(require 'deft)
+(setq
+  deft-extension "txt"
+  deft-directory deft-dir
+  deft-text-mode 'org-mode
+  deft-use-filename-as-title 't
+  visual-line-mode nil)
+(global-set-key (kbd "<f9>") 'deft)
 
 
 ;; CSS mode
@@ -112,10 +172,6 @@
 (setq auto-mode-alist
       (append '(("\\.js$" . javascript-mode)) auto-mode-alist))
 
-;; Use aspell instead of ispell
-(setq ispell-program-name "C:/Program Files (x86)/Aspell/bin/aspell.exe")
-(setq ispell-really-aspell t)
-(setq ispell-extra-args '("--sug-mode=fast"))
 
 ;;(flyspell-mode 1)
 (add-hook 'text-mode-hook
@@ -126,12 +182,7 @@
           (lambda () (flyspell-mode 1)))
 ;;(add-hook 'diary-display-hook 'flyspell-mode)
 
-
-;; Set diary location
-(setq diary-file "C:/Users/Russell/SkyDrive/Dropbox/files/work/diary")
-
 (setq visible-bell t)
-
 
 ; This was bugging me with Deft, so I took it out
 ;;;(add-hook 'before-save-hook (lambda () (delete-trailing-whitespace)))
@@ -183,43 +234,10 @@
 ;;(require 'color-theme-solarized)
 ;;(color-theme-solarized-light)
 ;;(color-theme-snowish)
-(require 'deft)
-  (setq
-     deft-extension "txt"
-     deft-directory "c:/Users/Russell/SkyDrive/Dropbox/PlainText/"
-     deft-text-mode 'org-mode
-     deft-use-filename-as-title 't
-     visual-line-mode nil)
-  (global-set-key (kbd "<f9>") 'deft)
 
-;; Add line numbers, just for Ruby for the time being
-;; Using my initials to avoid possible namespace collisions
-(require 'linum)
-(add-hook 'rdm-code-modes-hook
-          (lambda () (linum-mode 1)))
-(add-hook 'ruby-mode-hook
-          (lambda () (run-hooks 'rdm-code-modes-hook)))
-(add-hook 'html-mode-hook
-          (lambda () (run-hooks 'rdm-code-modes-hook)))
-(add-hook 'text-mode-hook
-          (lambda () (run-hooks 'rdm-code-modes-hook)))
 
-;; "y or n" instead of "yes or no"
-(fset 'yes-or-no-p 'y-or-n-p)
 
-;; Explicitly show the end of a buffer
-(set-default 'indicate-empty-lines t)
 
-;;; Highlight current line
-(global-hl-line-mode 1)
-;; Great for white background
-(set-face-background 'hl-line "lemon chiffon")
-
-;;; nxhtml mode
-;(load "c:/Users/Russell/.emacs.d/nxhtml/autostart.el")
-
-;;; Auto revert mode
-(global-auto-revert-mode 1)
 
 ;(require 'package)
 ;(add-to-list 'package-archives 
@@ -237,8 +255,7 @@
 ;   (sp-local-pair "<" ">")
 ;   (sp-local-pair "<%" "%>"))
 
-(find-file "c:/Users/Russell/SkyDrive/Dropbox/org/tasks.org")
-(find-file "c:/Users/Russell/SkyDrive/Dropbox/org/personal.org")
+
 
 (provide 'el-rusticus)
 ;;; el-rusticus.el ends here
