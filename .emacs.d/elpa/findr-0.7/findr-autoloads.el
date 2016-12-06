a7ee30852ff4927f9e00c620264d8d6592a8984c
Russell <russ.may@gmail.com>
2016-12-06 15:56:37 -0700

    Changing emacs configs

--- .emacs.d/elpa/findr-0.7/findr-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (20946 60472
-;;;;;;  192000 0))
-
-;;;***
-
-(provide 'findr-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; findr-autoloads.el ends here
